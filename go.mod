01d4fea93261556dc4f752969c401df2ca497905
Jonathan <48375298+JonathanDowdell@users.noreply.github.com>
2023-11-28 20:29:21 -0800

    📦 chore(go.mod): update dependencies and add support for google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 and google.golang.org/protobuf v1.31.0 📄 feat(proto/repo_service.proto): add proto file for RepoService with message and service definitions 🔨 refactor(public/model.go): add 'Description' field to PublicRepoModel to match the changes in the proto file

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/Bit-Bridge-Source/BitBridge-RepoService-Go
 go 1.21.3
 
 require (
-    go.mongodb.org/mongo-driver v1.12.1
-    github.com/Bit-Bridge-Source/BitBridge-CommonService-Go v1.10.4
+	github.com/Bit-Bridge-Source/BitBridge-CommonService-Go v1.10.4
+	go.mongodb.org/mongo-driver v1.12.1
+)
+
+require (
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
 )
 
 replace github.com/Bit-Bridge-Source/BitBridge-CommonService-Go => ../common-service
--- public/model.go
+++ public/model.go
@@ -7,10 +7,11 @@ import (
 )
 
 type PublicRepoModel struct {
-	ID        primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	Name      string             `json:"name" bson:"name"`
-	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
+	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	Name        string             `json:"name" bson:"name"`
+	Description string             `json:"description" bson:"description"`
+	CreatedAt   time.Time          `json:"created_at" bson:"created_at"`
+	UpdatedAt   time.Time          `json:"updated_at" bson:"updated_at"`
 }
 
 type CreateRepoModel struct {
